5824b268978093bbb6eabe769e588b51e2bb3845
Iveta Círová <cirova.iveta@ptw.cz>
2024-06-14 13:15:32 +0200

    vypnuto automatické zoomování mapy

--- src/components/Map.jsx
+++ src/components/Map.jsx
@@ -2,16 +2,22 @@ import "./Map.scss"
 import { MapContainer, TileLayer, Marker, Popup } from 'react-leaflet';
 import 'leaflet/dist/leaflet.css';
 import CustomMapPin from "./CustomMapPin";
+import { useEffect, useRef } from "react";
 
 
 const Map= ({locations}) => {
- 
+
+
   return (
     <div>
-     
-      
-      <MapContainer center={[49.9066213, 15.8054397]} zoom={7} style={{ height: '400px', width: '100%' }}>
+      <MapContainer 
+        center={[49.9066213, 15.8054397]} 
+        zoom={7}
+        style={{ height: '400px', width: '100%' }}
+        scrollWheelZoom={false}
+        >
         <TileLayer url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png" />
+        
         {locations.map((location) => (
           <Marker key={location.id} position={[location.lat, location.lng]} icon={CustomMapPin}>
             <Popup>
@@ -21,8 +27,6 @@ const Map= ({locations}) => {
           </Marker>
         ))}
       </MapContainer>
-
-     
     </div>
   );
 };
